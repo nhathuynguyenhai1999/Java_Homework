5c2c9bc22e6f316fe3315123c1ee72280b2402b9
nhathuynguyenhai1999 <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-15 11:28:51 +0700

    upload my file

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("Giá trị của biến: " + i);
+        float f = 20.5f;
+        System.out.println("Giá trị của biến: " + f);
+        double d = 20.5;
+        System.out.println("Giá trị của biến: " + d);
+        boolean b = true;
+        System.out.println("Giá trị của biến: " + b);
+        char c = 'a';
+        System.out.println("Giá trị của biến: " + c);
+        String s = "Hà Nội";
+        System.out.println("Giá trị của biến: " + s);
+    }
+}
