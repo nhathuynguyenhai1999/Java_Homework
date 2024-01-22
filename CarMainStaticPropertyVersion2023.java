15036be41c0c3dbd0dda0da55fdc12ee01bddd12
nhathuynguyenhai1999 <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-22 08:19:19 +0700

    update code and files

---
+++ CarMainStaticPropertyVersion2023.java
@@ -0,0 +1,16 @@
+public class CarMainStaticPropertyVersion2023 {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+    }
+}
