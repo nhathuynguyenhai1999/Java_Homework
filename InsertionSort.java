f884f3993f7fb3b78d4022c35955ea4e79b2bf3a
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-31 14:23:54 +0700

    upload my files

---
+++ InsertionSort.java
@@ -0,0 +1,47 @@
+package James_Module2;
+
+public class InsertionSort {
+    public static void insertionSortSteps(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+            printArray(arr, i);
+        }
+    }
+
+    public static void printArray(int[] arr, int step) {
+        System.out.print("Bước " + step + ": ");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Thay đổi mảng đầu vào tại đây
+        int[] inputArray = {12, 11, 13, 5, 6};
+
+        System.out.print("Mảng ban đầu: ");
+        for (int value : inputArray) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        insertionSortSteps(inputArray);
+
+        System.out.print("Mảng đã sắp xếp: ");
+        for (int value : inputArray) {
+            System.out.print(value + " ");
+        }
+    }
+}
+
