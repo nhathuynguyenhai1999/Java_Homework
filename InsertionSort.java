0ed75ea70fe35bb15bf11d07ba2de9c94855a5d0
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-31 14:27:51 +0700

    update my code

--- InsertionSort.java
+++ InsertionSort.java
@@ -18,6 +18,21 @@ public class InsertionSort {
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
     public static void printArray(int[] arr, int step) {
         System.out.print("Bước " + step + ": ");
         for (int value : arr) {
@@ -39,6 +54,13 @@ public class InsertionSort {
         insertionSortSteps(inputArray);
 
         System.out.print("Mảng đã sắp xếp: ");
+        for (int value : inputArray) {
+            System.out.print(value + " ");
+        }
+        // Gọi phương thức insertionSort để sắp xếp mảng
+        insertionSort(inputArray);
+
+        System.out.print("\nMảng đã sắp xếp (sử dụng insertionSort): ");
         for (int value : inputArray) {
             System.out.print(value + " ");
         }
