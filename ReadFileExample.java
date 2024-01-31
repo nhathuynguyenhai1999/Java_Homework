ae9c719c2ab6251b8223800b40c82f525eb612ed
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-31 15:50:30 +0700

    update my files

--- ReadFileExample.java
+++ ReadFileExample.java
@@ -1,6 +1,7 @@
 package ReadingFileText;
-
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReadFileExample {
@@ -24,11 +25,57 @@ public class ReadFileExample {
             throw new RuntimeException(e);
         }
     }
+    public void writeFile(String filePath, int max){
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (Integer number : numbers) {
+            if (max < number) {
+                max = number;
+            }
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println("Nhập đường dẫn file: ");
         String path = scanner.nextLine();
         ReadFileExample readFileEx = new ReadFileExample();
         readFileEx.readFileText(path);
+        //
+        ReadFileExample readAndWriteFile = new ReadFileExample();
+        List<Integer> numbers = readAndWriteFile.readFile();
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+
+    private List<Integer> readFile() {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            String filePath = null;
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+        return numbers;
     }
 }
