63dc3d8b39cecb9aff8511866d52b2f0d45a355a
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-31 17:32:03 +0700

    upload my files

---
+++ ArrayExample.java
@@ -0,0 +1,31 @@
+package James_Module2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    static Scanner scanner = new Scanner(System.in);
+    public int[] createRandom() {
+        Random rd = new Random();
+        int[] arr = new int[100];
+        System.out.println("Danh sách phần tử của mảng: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.println(arr[i] + "");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        int[] arr = arrayExample.createRandom();
+        System.out.println("\\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
