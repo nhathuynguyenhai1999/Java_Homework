cd9f076a410a6d8bd48e4a014fca258aa2771f2e
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-02-23 15:36:33 +0700

    upload my files

---
+++ TestThread.java
@@ -0,0 +1,34 @@
+package Threading;
+class OddThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println("Odd: " + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class EvenThread extends Thread {
+    public void run() {
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println("Even: " + i);
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class TestThread {
+    public static void main(String[] args) {
+        OddThread oddThread = new OddThread();
+        EvenThread evenThread = new EvenThread();
+
+        oddThread.start();
+        evenThread.start();
+    }
+}
