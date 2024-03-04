68943959b6e4cdcf00eda5efc6dd8ea761c97c6f
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-03-04 08:34:44 +0700

    upload my files

--- FactoryMethod.java
+++ FactoryMethod.java
@@ -26,6 +26,61 @@ public class FactoryMethod {
             }
         }
     }
+    // Shape interface
+    interface Shape {
+        void draw();
+    }
+
+    // Circle class
+    class Circle implements Shape {
+        @Override
+        public void draw() {
+            System.out.println("Drawing a Circle");
+        }
+    }
+
+    // Rectangle class
+    class Rectangle implements Shape {
+        @Override
+        public void draw() {
+            System.out.println("Drawing a Rectangle");
+        }
+    }
+
+    // Square class
+    class Square implements Shape {
+        @Override
+        public void draw() {
+            System.out.println("Drawing a Square");
+        }
+    }
+
+    // Shape Factory interface
+    interface ShapeFactory {
+        Shape createShape();
+    }
+
+    // Circle Factory
+    class CircleFactory implements ShapeFactory {
+        @Override
+        public Shape createShape() {
+            return new Circle();
+        }
+    }
+    // Rectangle Factory
+    class RectangleFactory implements ShapeFactory {
+        @Override
+        public Shape createShape() {
+            return new Rectangle();
+        }
+    }
+    // Square Factory
+    class SquareFactory implements ShapeFactory {
+        @Override
+        public Shape createShape() {
+            return new Square();
+        }
+    }
     static class FactoryDemo {
         public void main1(String[] args) {
             AnimalFactory animalFactory = new AnimalFactory();
@@ -38,5 +93,23 @@ public class FactoryMethod {
     public static void main(String[] args) {
         FactoryDemo factoryDemo = new FactoryDemo();
         factoryDemo.main1(args);
+    }
+    // Main class
+    public class FactoryMethodExample {
+        public void main1(String[] args) {
+            // Create a circle using the CircleFactory
+            ShapeFactory circleFactory = new CircleFactory();
+            Shape circle = circleFactory.createShape();
+            circle.draw();
+            // Create a rectangle using the RectangleFactory
+            ShapeFactory rectangleFactory = new RectangleFactory();
+            Shape rectangle = rectangleFactory.createShape();
+            rectangle.draw();
+            // Create a square using the SquareFactory
+            ShapeFactory squareFactory = new SquareFactory();
+            Shape square = squareFactory.createShape();
+            square.draw();
+
+        }
     }
 }
